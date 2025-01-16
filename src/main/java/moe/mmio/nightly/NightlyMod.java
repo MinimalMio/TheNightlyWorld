539da14467412718e7c6af0bb105c001319cbc7d
MinimalMio <yukati2024@outlook.com>
2025-01-16 16:32:46 +0000

    [feat]: new item and blocks!

--- src/main/java/moe/mmio/nightly/NightlyMod.java
+++ src/main/java/moe/mmio/nightly/NightlyMod.java
@@ -4,17 +4,17 @@ import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import moe.mmio.nightly.creativetabs.ModCreativeTabs;
+import moe.mmio.nightly.items.ModItems;
 
-@Mod(modid = NightlyMod.MODID, version = NightlyMod.VERSION)
-public class NightlyMod
-{
+@Mod(modid = NightlyMod.MODID, version = NightlyMod.VERSION, name = NightlyMod.MODNAME)
+public class NightlyMod {
     public static final String MODID = "nightly";
     public static final String VERSION = "1.0";
+    public static final String MODNAME = "The Nightly World";
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    public void init(FMLInitializationEvent event) {
+      ModItems.mainRegistry();
     }
 }
