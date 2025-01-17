67f3612bf2d74098777fed584f08a3ba877eaa98
MinimalMio <yukati2024@outlook.com>
2025-01-17 14:39:49 +0000

    [feat]: add the gamerule \'foreverNight\'

--- src/main/java/moe/mmio/nightly/NightlyMod.java
+++ src/main/java/moe/mmio/nightly/NightlyMod.java
@@ -1,20 +1,46 @@
 package moe.mmio.nightly;
 
 import net.minecraft.init.Blocks;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import moe.mmio.nightly.creativetabs.ModCreativeTabs;
 import moe.mmio.nightly.items.ModItems;
 
 @Mod(modid = NightlyMod.MODID, version = NightlyMod.VERSION, name = NightlyMod.MODNAME)
 public class NightlyMod {
-    public static final String MODID = "nightly";
-    public static final String VERSION = "1.0";
-    public static final String MODNAME = "The Nightly World";
+  public static final String MODID = "nightly";
+  public static final String VERSION = "1.0";
+  public static final String MODNAME = "The Nightly World";
 
-    @EventHandler
-    public void init(FMLInitializationEvent event) {
-      ModItems.mainRegistry();
+  @EventHandler
+  public void preInit(FMLPreInitializationEvent event) {
+    GameRules gameRules = MinecraftServer.getServer().worldServers[0].getGameRules();
+    if (!gameRules.hasRule("foreverNight")) {
+      gameRules.setOrCreateGameRule("foreverNight", "true");
     }
+  }
+
+  @EventHandler
+  public void init(FMLInitializationEvent event) {
+    /* Main Registry */
+    ModItems.mainRegistry();
+
+    /* Listen on game rules and control times. */
+    MinecraftServer server = MinecraftServer.getServer();
+    if (server != null && server.isSinglePlayer()) {
+      World world = server.worldServers[0];
+      if (world != null) {
+        GameRules gameRules = world.getGameRules();
+        if (gameRules.getGameRuleBooleanValue("foreverNight")) {
+          // set time to 18000(midnight)
+          world.setWorldTime(18000);
+        }
+      }
+    }
+  }
 }
