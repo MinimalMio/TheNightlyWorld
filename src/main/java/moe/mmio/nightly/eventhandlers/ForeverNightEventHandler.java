5a37cd15aa9122329c3d3d911fc40acd26454a8a
MinimalMio <yukati2024@outlook.com>
2025-01-18 11:37:16 +0000

    [fix]: 最终修....复？

--- src/main/java/moe/mmio/nightly/eventhandlers/ForeverNightEventHandler.java
+++ src/main/java/moe/mmio/nightly/eventhandlers/ForeverNightEventHandler.java
@@ -8,12 +8,13 @@ import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 
 public class ForeverNightEventHandler {
-
   @SubscribeEvent
   public void onWorldTick(TickEvent.WorldTickEvent event) {
+    /*
     if (!event.world.isRemote && event.world instanceof WorldServer) {
       WorldServer worldServer = (WorldServer) event.world;
       GameRules gameRules = worldServer.getGameRules();
@@ -28,6 +29,18 @@ public class ForeverNightEventHandler {
         }
       } else {
         gameRules.setOrCreateGameRule("doDaylightCycle", "true");
+      }
+    }
+    */
+    if (event.phase == TickEvent.Phase.START) {
+      World world = event.world;
+
+      if (!world.isRemote && world instanceof WorldServer) {
+        boolean foreverNight = world.getGameRules().getGameRuleBooleanValue("foreverNight");
+
+        if (foreverNight) {
+          world.setWorldTime(18000);
+        }
       }
     }
   }
